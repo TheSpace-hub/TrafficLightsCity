9f0ce5368b7f4b340dc9646a5588830470b7402d
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-10 01:03:27 +0300

    Смещение камеры

--- src/sprites/field.py
+++ src/sprites/field.py
@@ -20,19 +20,22 @@ class Field(Sprite):
         self.pixel_size = 10
 
         self.camera_distance: int = 10
+        self.camera_offset: tuple[int, int] = (0, 0)
 
         self.update_view()
 
     def update_view(self):
         self.image.fill((32, 32, 32))
-        for i in range(10):
+        for y in range(10):
             tile = Tile(self.game, self.tile_size, int(self.pixel_size * self.camera_distance / 10), TileTexture.GRASS)
             self.image.blit(tile.image,
                             (
-                                i * int(self.tile_size * self.pixel_size * self.camera_distance / 10 * cos(
-                                    radians(self._perspective_angle))),
-                                i * int(self.tile_size * self.pixel_size * self.camera_distance / 10 * sin(
-                                    radians(self._perspective_angle)))))
+                                self.camera_offset[0] + y * int(
+                                    self.tile_size * self.pixel_size * self.camera_distance / 10 * cos(
+                                        radians(self._perspective_angle))),
+                                self.camera_offset[1] + y * int(
+                                    self.tile_size * self.pixel_size * self.camera_distance / 10 * sin(
+                                        radians(self._perspective_angle)))))
 
     def update(self):
         pass
