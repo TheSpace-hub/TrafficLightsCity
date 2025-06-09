c7db7044a2bdfdba120b131d4d9e7af6f9e642e4
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-10 00:31:39 +0300

    Построение поля по прямой

--- src/sprites/field.py
+++ src/sprites/field.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING
+from math import sin, cos, sqrt, radians
 
 from src.sprite import Sprite
 from src.sprites import Tile, TileTexture
@@ -15,11 +16,17 @@ class Field(Sprite):
     def __init__(self, game: 'Game'):
         self._perspective_angle = 30
         super().__init__(game, (1920, 1080), (0, 0))
+        self.tile_size = 10
+        self.pixel_size = 10
+
         self.update_view()
 
     def update_view(self):
-        tile_1 = Tile(self.game, 10, 10, TileTexture.GRASS)
-        self.image.blit(tile_1.image, (10, 10))
+        for i in range(10):
+            tile = Tile(self.game, 10, 10, TileTexture.GRASS)
+            self.image.blit(tile.image,
+                            (i * int(self.tile_size * self.pixel_size * cos(radians(self._perspective_angle))),
+                             int(i * self.tile_size * self.pixel_size * sin(radians(self._perspective_angle)))))
 
     def update(self):
         pass
