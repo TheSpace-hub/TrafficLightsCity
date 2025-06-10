e896d73f6d921c0c59fdf93e0e411f35eb15f5fa
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-10 16:09:55 +0300

    Вынес получение цвета в enum

--- src/sprites/tile.py
+++ src/sprites/tile.py
@@ -14,6 +14,22 @@ if TYPE_CHECKING:
 class TileTexture(Enum):
     GRASS = 0
 
+    @classmethod
+    def get_one_of_colors(cls, texture: int) -> tuple[int, int, int]:
+        if texture == TileTexture.GRASS.value:
+            color = random.choice([
+                (58, 140, 62),
+                (94, 124, 22),
+                (102, 162, 24),
+                (58, 109, 53)
+            ])
+            return (
+                min(255, max(0, color[0] + random.randint(-20, 20))),
+                min(255, max(0, color[1] + random.randint(-20, 20))),
+                min(255, max(0, color[2] + random.randint(-20, 20)))
+            )
+        return 255, 255, 255
+
 
 class Tile(Sprite):
     """
@@ -36,22 +52,11 @@ class Tile(Sprite):
     def update_view(self):
         for y in range(self.size):
             for x in range(self.size):
-                color = random.choice([
-                    (58, 140, 62),
-                    (94, 124, 22),
-                    (102, 162, 24),
-                    (58, 109, 53)
-                ])
-                color = (
-                    min(255, max(0, color[0] + random.randint(-20, 20))),
-                    min(255, max(0, color[1] + random.randint(-20, 20))),
-                    min(255, max(0, color[2] + random.randint(-20, 20)))
-                )
                 start: tuple[float, float] = (
                     x * self.pixel_size * sqrt(3) / 2 + y * self.pixel_size * sqrt(3) / 2,
                     -x * self.pixel_size / 2 + y * self.pixel_size / 2
                 )
-                pg.draw.polygon(self.image, color, [
+                pg.draw.polygon(self.image, TileTexture.get_one_of_colors(0), [
                     [start[0],
                      start[1] + self.image.get_size()[1] / 2],
                     [start[0] + self.pixel_size * sqrt(3) / 2,
