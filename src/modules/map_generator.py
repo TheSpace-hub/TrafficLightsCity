899278777afebddc3dd6b6fc0587bbde02cc1346
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-14 02:00:57 +0300

    Добавил улучшенную проверку на возможность построить дорогу

--- src/modules/map_generator.py
+++ src/modules/map_generator.py
@@ -10,13 +10,39 @@ class Direction(Enum):
     TOP = (0, -1)
     BOTTOM = (0, 1)
 
+    @staticmethod
+    def get_territory_that_should_be_empty(direction: tuple[int, int]):
+        variants: dict[tuple[int, int], list[tuple[int, int]]] = {
+            Direction.RIGHT.value: [
+                (1, -1), (2, -1),
+                (1, 0), (2, 0),
+                (1, 1), (2, 1)
+            ],
+            Direction.LEFT.value: [
+                (-1, -1), (-2, -1),
+                (-1, 0), (-2, 0),
+                (-1, 1), (-2, 1)
+            ],
+            Direction.TOP.value: [
+                (-1, 1), (-1, 2),
+                (0, 1), (0, 2),
+                (2, 1), (1, 2)
+            ],
+            Direction.BOTTOM.value: [
+                (-1, -1), (-1, -2),
+                (0, -1), (0, -2),
+                (2, -1), (1, -2)
+            ]
+        }
+        return variants[direction]
+
 
 class MapGenerator:
     def __init__(self, size: tuple[int, int]):
         if size[0] < 3 or size[1] < 3:
             logging.error('Поле не может быть размером меньше 3x3. (Размер поля: %s)', size)
         self._field: dict[tuple[int, int], TileTexture] = {}
-        self._construction_points: list[tuple[int, int]] = []
+        self._construction_points: list[tuple[int, int]] = [(round(size[0] / 2), round(size[1] // 2))]
         self._size: tuple[int, int] = size
 
     def generate_map(self) -> dict[tuple[int, int], TileTexture]:
@@ -25,14 +51,9 @@ class MapGenerator:
                 self._field[(x, y)] = TileTexture.GRASS
         self._field[(round(self._size[0] / 2), round(self._size[1] // 2))] = TileTexture.ASPHALT
 
-        self._construction_points.append((round(self._size[0] / 2) + 1, round(self._size[1] // 2)))
-        self._construction_points.append((round(self._size[0] / 2) - 1, round(self._size[1] // 2)))
-        self._construction_points.append((round(self._size[0] / 2), round(self._size[1] // 2) + 1))
-        self._construction_points.append((round(self._size[0] / 2), round(self._size[1] // 2) - 1))
-
-        # while len(self._construction_points) > 0:
-        #     for point in self._construction_points[:]:
-        #         self._build_something_from_point(point)
+        while len(self._construction_points) > 0:
+            for point in self._construction_points[:]:
+                self._build_something_from_point(point)
 
         return self._field
 
@@ -40,6 +61,11 @@ class MapGenerator:
         if point not in self._construction_points:
             logging.warning('Точки %s нет в списке точек для построения', point)
             return
+
+        for direction in self._get_possible_construction_directions(point):
+            self._field[(point[0] + direction.value[0], point[1] + direction.value[1])] = TileTexture.ASPHALT
+            self._construction_points.append((point[0] + direction.value[0], point[1] + direction.value[1]))
+        self._construction_points.remove(point)
 
     def _get_possible_construction_directions(self, point: tuple[int, int]) -> list[Direction]:
         if point not in self._construction_points:
@@ -49,15 +75,13 @@ class MapGenerator:
         directions: list[Direction] = []
         for direction in [Direction.RIGHT, Direction.LEFT, Direction.TOP, Direction.BOTTOM]:
             can_build: bool = True
-            for x in range(point[0] - 1 + direction.value[0], point[0] + 2 + direction.value[0]):
-                if not can_build:
+            for check_point in Direction.get_territory_that_should_be_empty(direction.value):
+                x: int = point[0] + check_point[0]
+                y: int = point[1] + check_point[1]
+                if not (0 < x < self._size[0]) or not (0 < y < self._size[1]) or self._field[
+                    (x, y)] != TileTexture.GRASS:
+                    can_build = False
                     break
-                for y in range(point[1] - 1 + direction.value[1], point[1] + 2 + direction.value[1]):
-                    if (x, y) == point:
-                        continue
-                    if self._field[(x, y)] != TileTexture.GRASS:
-                        can_build = False
-                        break
             if can_build:
                 directions.append(direction)
         return directions
