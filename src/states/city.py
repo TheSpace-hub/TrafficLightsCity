c7c0c6278883d5bcc9d7e3504c231b211461fbd4
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-15 00:34:24 +0300

    Убрал карту и добавил тайл на время дебигинга

--- src/states/city.py
+++ src/states/city.py
@@ -3,7 +3,7 @@ import pygame as pg
 
 from src.state import State
 
-from src.sprites import Text, TextAlign, Field
+from src.sprites import Text, TextAlign, Field, Tile, TileTexture
 
 if TYPE_CHECKING:
     from src.game import Game
@@ -14,7 +14,9 @@ class City(State):
         super().__init__(game)
 
     def boot(self):
-        self.add_sprite('field', Field(self.game))
+        # self.add_sprite('field', Field(self.game))
+
+        self.add_sprite('tile', Tile(self.game, 9, 10, TileTexture.GRASS, 0.52))
 
         self.add_sprite('city_name', Text(self.game, (10, 10), 'City.01', 16,
                                           (255, 255, 255), align=TextAlign.LEFT))
@@ -23,44 +25,46 @@ class City(State):
                                                            (255, 255, 255), align=TextAlign.LEFT))
 
     def update(self):
-        if 5 in self.game.omitted_mouse_buttons:
-            field: Field = self.get_sprite('field')
-            field.change_camera_distance(field.get_camera_distance() - 1)
-        elif 4 in self.game.omitted_mouse_buttons:
-            field: Field = self.get_sprite('field')
-            field.change_camera_distance(field.get_camera_distance() + 1)
-
-        if (pg.key.get_pressed()[pg.K_w] or pg.key.get_pressed()[pg.K_s] or pg.key.get_pressed()[pg.K_a] or
-                pg.key.get_pressed()[pg.K_d]):
-            field: Field = self.get_sprite('field')
-            direction: dict[int, tuple[int, int]] = {
-                pg.K_w: (0, 1),
-                pg.K_s: (0, -1),
-                pg.K_a: (1, 0),
-                pg.K_d: (-1, 0)
-            }
-            for key in [pg.K_w, pg.K_s, pg.K_a, pg.K_d]:
-                if pg.key.get_pressed()[key]:
-                    field.camera_offset = (
-                        field.camera_offset[0] + int(
-                            direction[key][0] * field.move_speed * field.get_camera_distance() / 10),
-                        field.camera_offset[1] + int(
-                            direction[key][1] * field.move_speed * field.get_camera_distance() / 10))
-            field.update_view()
-
-        field: Field = self.get_sprite('field')
-        debug_camera_distance_text: Text = self.get_sprite('debug_camera_distance_text')
-        debug_camera_distance_text.text = str(field.get_camera_distance())
-        debug_camera_distance_text.update_view()
+        pass
+        # if 5 in self.game.omitted_mouse_buttons:
+        #     field: Field = self.get_sprite('field')
+        #     field.change_camera_distance(field.get_camera_distance() - 1)
+        # elif 4 in self.game.omitted_mouse_buttons:
+        #     field: Field = self.get_sprite('field')
+        #     field.change_camera_distance(field.get_camera_distance() + 1)
+        #
+        # if (pg.key.get_pressed()[pg.K_w] or pg.key.get_pressed()[pg.K_s] or pg.key.get_pressed()[pg.K_a] or
+        #         pg.key.get_pressed()[pg.K_d]):
+        #     field: Field = self.get_sprite('field')
+        #     direction: dict[int, tuple[int, int]] = {
+        #         pg.K_w: (0, 1),
+        #         pg.K_s: (0, -1),
+        #         pg.K_a: (1, 0),
+        #         pg.K_d: (-1, 0)
+        #     }
+        #     for key in [pg.K_w, pg.K_s, pg.K_a, pg.K_d]:
+        #         if pg.key.get_pressed()[key]:
+        #             field.camera_offset = (
+        #                 field.camera_offset[0] + int(
+        #                     direction[key][0] * field.move_speed * field.get_camera_distance() / 10),
+        #                 field.camera_offset[1] + int(
+        #                     direction[key][1] * field.move_speed * field.get_camera_distance() / 10))
+        #     field.update_view()
+        #
+        # field: Field = self.get_sprite('field')
+        # debug_camera_distance_text: Text = self.get_sprite('debug_camera_distance_text')
+        # debug_camera_distance_text.text = str(field.get_camera_distance())
+        # debug_camera_distance_text.update_view()
 
     def enter(self):
-        field: Field = self.get_sprite('field')
-
-        seed = None
-        if 'seed' in self.game.transmitted_data and type(self.game.transmitted_data['seed']) == int:
-            seed = self.game.transmitted_data['seed']
-        field.generate_field(seed)
-        field.update_view()
+        pass
+        # field: Field = self.get_sprite('field')
+        #
+        # seed = None
+        # if 'seed' in self.game.transmitted_data and type(self.game.transmitted_data['seed']) == int:
+        #     seed = self.game.transmitted_data['seed']
+        # field.generate_field(seed)
+        # field.update_view()
 
     def exit(self):
         pass
