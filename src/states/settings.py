f2897e5c4a8454e7655daf659f98cd43bca4c992
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-17 17:51:56 +0300

    Добавит тестовые опции

--- src/states/settings.py
+++ src/states/settings.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING
 
 from src.state import State
 
-from src.sprites import Button, InBlockText, ButtonStatus, ChoiceOfSeveralOptions
+from src.sprites import Button, InBlockText, ButtonStatus, ChoiceOfSeveralOptions, Option
 
 if TYPE_CHECKING:
     from src.game import Game
@@ -17,8 +17,14 @@ class Settings(State):
                                        InBlockText(self.game, 'Назад', 16,
                                                    (255, 255, 255)),
                                        self.on_back_button_pressed))
+
+        options: list[Option] = [
+            Option(InBlockText(self.game, 'Test option 1', 16, (255, 255, 255)), 'Test1'),
+            Option(InBlockText(self.game, 'Test option 2', 16, (255, 255, 255)), 'Test2'),
+            Option(InBlockText(self.game, 'Test option 3', 16, (255, 255, 255)), 'Test3'),
+        ]
         self.add_sprite('graphics_quality', ChoiceOfSeveralOptions(self.game, (100, 100), (500, 70),
-                                                                   []))
+                                                                   options))
 
     def update(self):
         pass
