b910e3fe014f4153e3f0600fc616350c396df7c0
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-23 14:11:58 +0500

    кнопка доступна только при правильном наборе текста и существовании картинок

--- src/states/traffic_light_texture_editor.py
+++ src/states/traffic_light_texture_editor.py
@@ -16,9 +16,19 @@ class TrafficLightTextureEditor(State):
         self.current_container: int = 0
 
     def boot(self):
-        self.add_sprite('add_texture_button', Button(self.game, 100, 50, 400, 70,
+        self.add_sprite('texture_name_input', Input(self.game, 100, 50, 700, 70,
+                                                    InBlockText(self.game, '', 16, (255, 255, 255)),
+                                                    InBlockText(self.game, 'Введите название текстуры', 16,
+                                                                (255, 255, 255))
+                                                    ))
+        self.add_sprite('add_texture_button', Button(self.game, 100, 130, 400, 70,
                                                      InBlockText(self.game, 'Добавить изображение', 16,
                                                                  (255, 255, 255)), self.on_create_world_button_pressed))
+        self.add_sprite('create_texture_button', Button(self.game, 510, 130, 400, 70,
+                                                        InBlockText(self.game, 'Создать текстуру', 16,
+                                                                    (255, 255, 255)),
+                                                        self.on_create_world_button_pressed,
+                                                        False))
 
     def on_create_world_button_pressed(self, status: ButtonStatus):
         if status == ButtonStatus.PRESSED:
@@ -28,12 +38,12 @@ class TrafficLightTextureEditor(State):
             )
             image_name: str = file_path.split('/')[-1:][0].split('.')[0].lower()
             self.add_sprite(f'container_pixelart_{self.current_container}',
-                            Container(self.game, (100, 150 + 110 * self.current_container), (500, 100)))
+                            Container(self.game, (100, 210 + 110 * self.current_container), (500, 100)))
             self.add_sprite(f'pixelart_{self.current_container}',
-                            Pixelart(self.game, (110, 160 + 110 * self.current_container), (80, 80),
+                            Pixelart(self.game, (110, 220 + 110 * self.current_container), (80, 80),
                                      Pixelart.get_pixelart_by_image(file_path)))
             self.add_sprite(f'pixelart_name_input_{self.current_container}',
-                            Input(self.game, 200, 165 + 110 * self.current_container, 380, 70,
+                            Input(self.game, 200, 225 + 110 * self.current_container, 380, 70,
                                   InBlockText(self.game, image_name, 16, (255, 255, 255)),
                                   InBlockText(self.game, 'Введите название', 16, (255, 255, 255))
                                   ))
@@ -41,7 +51,9 @@ class TrafficLightTextureEditor(State):
             self.current_container += 1
 
     def update(self):
-        pass
+        texture_name_input: Input = self.get_sprite('texture_name_input')
+        create_texture_button: Button = self.get_sprite('create_texture_button')
+        create_texture_button.enabled = self.current_container > 0 and texture_name_input.text.text != ''
 
     def enter(self):
         pass
