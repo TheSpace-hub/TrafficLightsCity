9f7e82901895201dd1bc19c6a26d2dc7f595efd8
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-29 20:01:41 +0500

    Список всех id светофоров

--- src/states/dashboard.py
+++ src/states/dashboard.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 from src.state import State
 
-from src.sprites import TrafficLight
+from src.sprites import TrafficLight, Container, Text, TextAlign
 
 if TYPE_CHECKING:
     from src.game import Game
@@ -10,9 +10,24 @@ if TYPE_CHECKING:
 class Dashboard(State):
     def __init__(self, game: 'Game'):
         super().__init__(game)
+        self.traffic_lights: list[TrafficLight] = [
+            TrafficLight(self.game, 'moscow_1', 'basic'),
+            TrafficLight(self.game, 'moscow_2', 'basic'),
+            TrafficLight(self.game, 'moscow_3', 'arrow'),
+            TrafficLight(self.game, 'moscow_4', 'arrow')
+        ]
 
     def boot(self):
-        self.add_sprite('tfl', TrafficLight(self.game, 'arrow'))
+        self.add_sprite('traffic_lights_id_container', Container(self.game, (0, 0), (400, 1080)))
+        self.add_sprite('traffic_lights_id_text', Text(self.game, (10, 10), 'Список ID светофоров:', 18,
+                                                       (255, 255, 255), align=TextAlign.LEFT))
+        self._add_traffic_lights_ids()
+
+    def _add_traffic_lights_ids(self):
+        for i in range(len(self.traffic_lights)):
+            self.add_sprite(f'traffic_light_{i}_id',
+                            Text(self.game, (30, 50 + 30 * i), self.traffic_lights[i].data.uuid,
+                                 16, (255, 255, 255), align=TextAlign.LEFT))
 
     def update(self):
         pass
