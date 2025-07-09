d8dc9ebc7cf7f0964cf27ddf246989726c2f2217
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-09 12:45:42 +0500

    Поправил получение позиции

--- src/sprites/tile_selection.py
+++ src/sprites/tile_selection.py
@@ -17,7 +17,7 @@ class TileSelection(Sprite):
 
     def update_view(self):
         self.image = pg.Surface((1920, 1080), pg.SRCALPHA, 32).convert_alpha()
-        coord: tuple[int, int] = self.get_coord()
+        coord: tuple[int, int] = self.field.get_offset_from_coordinates(self.get_coord())
         polygon: list[tuple[int, int]] = list(
             map(lambda pos: (pos[0] + coord[0], pos[1] + coord[1]), self.field.get_tile_as_polygon()))
         pg.draw.polygon(self.image, (255, 255, 255), polygon, 3)
@@ -30,9 +30,7 @@ class TileSelection(Sprite):
         self._visible = visible
 
     def get_coord(self) -> tuple[int, int]:
-        return self.field.get_offset_from_coordinates(
-            self.field.get_tile_position_by_coordinates(pg.mouse.get_pos())
-        )
+        return self.field.get_tile_position_by_coordinates(pg.mouse.get_pos())
 
     def update(self):
         if self._visible:
