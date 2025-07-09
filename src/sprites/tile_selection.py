91b60144bedd6cbc5916fbdb7e8761e0f3152fb6
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-09 12:31:51 +0500

    Вынес получение координат в отдельную функцию

--- src/sprites/tile_selection.py
+++ src/sprites/tile_selection.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 import pygame as pg
 
 from src.sprite import Sprite
@@ -9,16 +9,15 @@ if TYPE_CHECKING:
 
 
 class TileSelection(Sprite):
-    def __init__(self, game: 'Game', filed: 'Field'):
+    def __init__(self, game: 'Game', filed: 'Field', func: Callable[[tuple[int, int]], None] | None):
         super().__init__(game, (1920, 1080))
         self.field: 'Field' = filed
+        self.func: Callable[[tuple[int, int]], None] | None = func
         self._visible: bool = False
 
     def update_view(self):
         self.image = pg.Surface((1920, 1080), pg.SRCALPHA, 32).convert_alpha()
-        coord = self.field.get_offset_from_coordinates(
-            self.field.get_tile_position_by_coordinates(pg.mouse.get_pos())
-        )
+        coord: tuple[int, int] = self.get_coord()
         polygon: list[tuple[int, int]] = list(
             map(lambda pos: (pos[0] + coord[0], pos[1] + coord[1]), self.field.get_tile_as_polygon()))
         pg.draw.polygon(self.image, (255, 255, 255), polygon, 3)
@@ -30,6 +29,13 @@ class TileSelection(Sprite):
         self.image = pg.Surface((1920, 1080), pg.SRCALPHA, 32).convert_alpha()
         self._visible = visible
 
+    def get_coord(self) -> tuple[int, int]:
+        return self.field.get_offset_from_coordinates(
+            self.field.get_tile_position_by_coordinates(pg.mouse.get_pos())
+        )
+
     def update(self):
         if self._visible:
             self.update_view()
+            if 1 in self.game.omitted_mouse_buttons:
+                self.func(self.get_coord())
