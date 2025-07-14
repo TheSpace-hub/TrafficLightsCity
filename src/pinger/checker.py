2ca020337ababc6ace7a099d661c00499893f6df
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-15 02:10:17 +0500

    Класс для проверки светофора

---
+++ src/pinger/checker.py
@@ -0,0 +1,23 @@
+from typing import Callable
+
+
+class Checker:
+    def __init__(self):
+        self.functions: dict[str, Callable[[dict, dict], tuple[bool, str]]] = {}
+
+    def __call__(self, tfl_type):
+        def decorator(func):
+            self.functions[tfl_type] = func
+
+            def wrapper(request: dict, response: dict):
+                return func(request, response)
+
+            return wrapper
+
+        return decorator
+
+    def check(self, tfl_type: str, request: dict, response: dict) -> tuple[bool, str] | None:
+        for current_type, func in self.functions.items():
+            if tfl_type == current_type:
+                return func(request, response)
+        return None
