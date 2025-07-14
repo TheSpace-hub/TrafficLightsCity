3ea7e0634977973c89ff9108546c253fa6a2cd1f
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-15 01:53:02 +0500

    Добавил входные данные и описание функции

---
+++ check/master.py
@@ -0,0 +1,14 @@
+def check_basic(request: dict, response: dict) -> tuple[bool, str | None]:
+    """
+    Пример проверки запроса на светофор и ответа.
+
+    Args:
+        request (dict): Словарь, отправленный долбилкой
+        response (dict):  Словарь, возвращённый с сервиса
+
+    Returns:
+        tuple[bool, str]: Кортеж, где:
+            - bool: Результат проверки (True — успех, False — ошибка).
+            - str | None: Описание ошибки (Используйте None для отсутствия параметра)
+    """
+    return True, ''
