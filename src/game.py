fc58c94d8a7439a4a444a65bcb8e4fd8c9813eb7
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-15 09:22:54 +0500

    Добавил docstring

--- src/game.py
+++ src/game.py
@@ -1,10 +1,23 @@
-"""
-Основной класс игры.
-Отвечает за цикл игры и изменение состояний.
+"""Основной модуль игры 'Город светофоров'.
+
+Реализует главный игровой цикл, управление состояниями (сценами) и отрисовку.
+
+Модуль содержит:
+- Класс Game - ядро игры, управляющее всеми процессами
+- Настройку системы логирования
+- Инициализацию pygame
+- Механизм переключения между игровыми состояниями (сценами)
+
+Основные функции:
+- Запуск и остановка игры
+- Обработка ввода (клавиатура, мышь)
+- Обновление и отрисовка текущего состояния
+- Управление временем кадров (delta time)
+- Взаимодействие с сервером светофоров через Pinger
 """
 import os
 import logging
-from typing import Any
+from typing import Any, TypeVar, Type, Optional
 
 from colorlog import ColoredFormatter
 
@@ -17,10 +30,35 @@ from src.pinger import Pinger
 
 from src.states import *  # Импорт НЕ УДАЛЯТЬ. Нужен чтобы все дочерние классы State были инициализированны
 
+StateT = TypeVar('StateT', bound=State)
+
 
 class Game:
-    """
-    Основной класс игры
+    """Основной класс, управляющий игровым процессом.
+
+    Отвечает за:
+    - Главный игровой цикл
+    - Управление состояниями (сценами)
+    - Обработку ввода
+    - Синхронизацию с сервером светофоров
+    - Отрисовку игровых объектов
+
+    Attributes:
+        states (dict[str, State]): Словарь всех зарегистрированных состояний
+        current_state (State | None): Текущее активное состояние
+        transmitted_data (dict[str, Any]): Данные для передачи между состояниями
+        screen (Surface): Основная поверхность для отрисовки
+        clock (Clock): Таймер для контроля FPS
+        omitted_buttons (list[int]): Список пропущенных нажатий клавиш
+        omitted_mouse_buttons (list[int]): Список пропущенных нажатий мыши
+        is_mouse_move (bool): Флаг движения мыши
+        running (bool): Флаг работы игрового цикла
+        delta_time (float): Время между кадрами (в секундах)
+        lock_mouse (bool): Флаг блокировки курсора мыши
+        _previous_mouse_location (tuple[int, int]): Предыдущая позиция мыши
+        mouse_offset (tuple[int, int]): Смещение мыши с последнего кадра
+        pinger (Pinger): Клиент для взаимодействия с сервером светофоров
+        last_ping_time (float): Время последнего ping-запроса
     """
 
     def __init__(self):
@@ -54,8 +92,12 @@ class Game:
         self.init_states()
 
     def loop(self):
-        """
-        Основной цикл игры
+        """Запускает главный игровой цикл.
+
+        Обрабатывает:
+        - События pygame (выход, ввод с клавиатуры/мыши)
+        - Обновление состояния игры
+        - Отрисовку кадра
         """
         while self.running:
             self.omitted_buttons = []
@@ -84,8 +126,12 @@ class Game:
         pg.quit()
 
     def update(self):
-        """
-        Обновляет текущую сцену
+        """Выполняет обновление игрового состояния текущей сцены.
+
+        Обновляет:
+        - Логику текущего состояния (сцены)
+        - Все зарегистрированные спрайты сцены
+        - Отображение (через update_view())
         """
         if self.current_state:
             self.current_state.update()
@@ -95,8 +141,7 @@ class Game:
         self.update_view()
 
     def update_view(self):
-        """
-        Обновляет экран
+        """Отрисовывает все объекты на сцене.
         """
         x_factor = pg.display.get_window_size()[0] / 1920
         y_factor = pg.display.get_window_size()[1] / 1080
@@ -106,23 +151,48 @@ class Game:
         pg.display.flip()
 
     def init_states(self):
-        """
-        Добавить все сцены
+        """Инициализировать сцены по умолчанию.
         """
         for state in State.__subclasses__():
             self.register_state(state)
 
-    def register_state(self, state):
-        """
-        Добавление сцен
-        :param state: Объект сцены
+    def register_state(self, state: Type[StateT]):
+        """Регистрация новой сцены.
+
+        Args:
+            state (Type[StateT]): Класс-наследник абстрактного State.
+
+        Note:
+            Вызывает функцию boot().
         """
         self.states[str(state.__name__)] = state(self)
         self.states[str(state.__name__)].boot()
 
-    def change_state(self, state: str, transmitted_data: dict[str, Any] = None):
-        """
-        Изменить сцену
+    def change_state(self, state: str, transmitted_data: Optional[dict[str, Any]] = None):
+        """Переключает текущее активное состояние игры на указанное.
+
+        Args:
+            state: Название состояния для переключения (должно быть зарегистрировано
+                   через register_state()). Регистрозависимая строка.
+            transmitted_data: Данные для передачи в новое состояние. Будут доступны
+                             через self.transmitted_data в методе enter() нового состояния.
+                             По умолчанию None (используется пустой словарь).
+
+        Side Effects:
+            - Изменяет self.current_state
+            - Временно изменяет self.transmitted_data
+            - Вызывает методы exit() и enter() у состояний
+            - Логирует ошибки при отсутствии состояния
+
+        Note:
+            - transmitted_data очищается после вызова enter()
+            - Для корректной работы состояние должно быть предварительно зарегистрировано
+            - Метод безопасен для вызова (не выбрасывает исключения при ошибках)
+
+        Example:
+            >>> game.change_state("MainMenu", {"show_intro": True})
+            >>> game.current_state.__class__.__name__
+            'MainMenu'
         """
         if transmitted_data is None:
             transmitted_data = {}
@@ -139,13 +209,15 @@ class Game:
         self.transmitted_data = {}
 
     def quit(self):
+        """Завершение игрового цикла
+        """
         self.running = False
 
     @staticmethod
     def configure_logs():
         """
         Настройка логов.
-        Определяет место записи и цвет логов цветными.
+        Определяет место записи и определяет цвет логов.
         """
         os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
         logging.getLogger('werkzeug').disabled = True
