5d24df2df61f0e03affa6de075c978b1e3d60d06
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-15 10:24:42 +0500

    Логирование в пингере

--- src/pinger/pinger.py
+++ src/pinger/pinger.py
@@ -1,3 +1,4 @@
+import logging
 from typing import TYPE_CHECKING
 import json
 
@@ -19,6 +20,11 @@ class Pinger:
         self.traffic_lights_data.append(traffic_light)
 
     def ping(self) -> dict[str, tuple[int, dict]]:
+        try:
+            requests.get(f'http://{self.host}:{self.port}/traffic')
+        except requests.exceptions.ConnectionError:
+            logging.warning('Не удалось соединиться с сервисом. (GET-запрос на url: %s).',
+                         f'http://{self.host}:{self.port}/traffic')
         responses: dict[str, tuple[int, dict]] = {}
         for traffic_light in self.traffic_lights_data:
             responses[traffic_light.uuid] = self._ping_traffic_light(traffic_light)
@@ -26,15 +32,25 @@ class Pinger:
         self.current_time += 1
         return responses
 
-    def _ping_traffic_light(self, traffic_light: 'TrafficLightData') -> tuple[int, dict]:
-        response = requests.get(f'http://{self.host}:{self.port}/traffic', params={
-            'type': str(traffic_light.type_value),
-            'data': json.dumps({
-                'uuid': str(traffic_light.uuid),
-                'current_time': traffic_light.current_time,
-                'current_state': traffic_light.get_state() + 1
+    def _ping_traffic_light(self, data: 'TrafficLightData') -> tuple[int, dict]:
+        """Пинг отдельного светофора.
+        Args:
+            data: Данные светофора
+
+        """
+        try:
+            response = requests.get(f'http://{self.host}:{self.port}/traffic', params={
+                'type': str(data.type_value),
+                'data': json.dumps({
+                    'uuid': str(data.uuid),
+                    'current_time': data.current_time,
+                    'current_state': data.get_state() + 1
+                })
             })
-        })
-        traffic_light.current_time += 1
-        traffic_light.set_state(int(json.loads(response.content)['next_state']) - 1)
+        except requests.exceptions.ConnectionError:
+            logging.info('Не удалось соединиться с сервисом. (GET-запрос на url: %s). Учтено как 500-ка',
+                            f'http://{self.host}:{self.port}/traffic')
+            return 500, {}
+        data.current_time += 1
+        data.set_state(int(json.loads(response.content)['next_state']) - 1)
         return response.status_code, json.loads(response.content)
