25cdd6d15babc366ec85eb6dc60037666e7156aa
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-18 01:05:25 +0500

    Добавил docstring-и

--- src/states/city.py
+++ src/states/city.py
@@ -1,3 +1,5 @@
+"""Модуль сцены города.
+"""
 from typing import TYPE_CHECKING, Optional
 from math import pi
 import pygame as pg
@@ -14,11 +16,25 @@ if TYPE_CHECKING:
 
 
 class City(State):
+    """Сцена города.
+
+    Attributes:
+        selected_type_of_traffic_light_creation (Optional[str]): При нажатии на кнопку строительства
+            светофора здесь, сохраняется информация о нажатой кнопке
+    """
+
     def __init__(self, game: 'Game'):
+        """Создание сцены города.
+
+        Args:
+            game (Game): Экземпляр игры
+        """
         super().__init__(game)
         self.selected_type_of_traffic_light_creation: Optional[str] = ''
 
     def boot(self):
+        """Инициализация сцены
+        """
         filed: Field = self.add_sprite('field', Field(self.game))
 
         self.add_sprite('city_name', Text(self.game, (10, 10), 'City.01', 16,
@@ -34,11 +50,17 @@ class City(State):
         self.add_construction_management_elements_buttons()
 
     def update(self):
+        """Обновление сцены.
+
+        Реализация передвижения и ежесекундный пинг.
+        """
         self.movement()
         self.pinging()
 
     def pinging(self):
-        if time.time() - self.game.last_ping_time >= 1 and self.game.pinger.running:
+        """Пинг каждую секунду.
+        """
+        if time.time() - self.game.last_ping_time >= .1 and self.game.pinger.running:
             self.game.pinger.ping()
             self.game.last_ping_time = time.time()
 
@@ -46,19 +68,22 @@ class City(State):
             field.update_view()
 
     def add_construction_management_elements_buttons(self):
+        """Добавление кнопок, отвечающих за строительство.
+        """
         self.add_traffic_lights_build_buttons()
         self.add_cansel_building_button()
 
     def add_cansel_building_button(self):
+        """Добавление кнопки отмены строительства.
+        """
         self.add_sprite('cansel_building', Button(self.game, (10, 970), (100, 100),
                                                   InBlockText(self.game, 'стоп', 16, (255, 255, 255)),
                                                   enabled=False, func=self.on_cansel_build_button_pressed))
 
-    def add_traffic_lights_build_buttons(self, state: int = 0):
+    def add_traffic_lights_build_buttons(self):
+        """Добавление кнопок всех типов светофоров.
+        """
         traffic_lights: list[TrafficLight] = [TrafficLight(self.game, t) for t in TrafficLightData.get_all_types()]
-
-        for t in traffic_lights:
-            t.data.set_state(min(state, len(t.data.states) - 1))
 
         for i in range(len(traffic_lights)):
             self.add_sprite(f'traffic_light_{traffic_lights[i].data.tfl_type}_build',
@@ -69,6 +94,8 @@ class City(State):
                                    placeholder=traffic_lights[i].get_cover))
 
     def enter(self):
+        """Передача сида и размера карты при заходе.
+        """
         field_sizes: dict[str, tuple[int, int]] = {
             'small': (30, 30),
             'medium': (50, 50),
@@ -83,22 +110,33 @@ class City(State):
         self.game.pinger.running = True
 
     def on_dashboard_button_pressed(self, status: ButtonStatus):
+        """Переход на сцену при нажатии на кнопку "Панель управления".
+        """
         if status == ButtonStatus.PRESSED:
             self.game.change_state('Dashboard')
 
     def on_traffic_light_build_button_pressed(self, status: ButtonStatus, context: str):
+        """Выбор светофора при нажатии на кнопку постройки светофора.
+        """
         if status == ButtonStatus.PRESSED:
             self.selected_type_of_traffic_light_creation = context
             tile_selection: TileSelection = self.get_sprite('tile_selection')
             tile_selection.set_visible(True)
 
     def on_cansel_build_button_pressed(self, status: ButtonStatus):
+        """Действие при нажатии на кнопку отмены строительства.
+        """
         if status == ButtonStatus.PRESSED:
             self.selected_type_of_traffic_light_creation = None
             tile_selection: TileSelection = self.get_sprite('tile_selection')
             tile_selection.set_visible(False)
 
     def build_traffic_light(self, pos: tuple[int, int]):
+        """Постройка светофора на поле.
+
+        Args:
+            pos: Координаты
+        """
         field: Field = self.get_sprite('field')
         if not field.can_build_traffic_light(pos):
             return
@@ -113,6 +151,8 @@ class City(State):
         field.traffic_lights[pos].data = data
 
     def generate_uuid(self) -> str:
+        """Создание более интересного уникального uuid для светофора.
+        """
         cities: list[str] = [
             "moscow", "saintpetersburg", "novosibirsk", "yekaterinburg", "kazan",
             "nizhnynovgorod", "chelyabinsk", "samara", "omsk", "rostovondon",
@@ -149,6 +189,8 @@ class City(State):
         return uuid
 
     def movement(self):
+        """Логика передвижения.
+        """
         field: Field = self.get_sprite('field')
         if pg.key.get_pressed()[pg.K_u]:
             field.perspective_angle = min(field.perspective_angle + 0.1, pi / 4)
@@ -187,4 +229,6 @@ class City(State):
             tile_selection.set_visible(False)
 
     def exit(self):
+        """Остановка пингера при выходе.
+        """
         self.game.pinger.running = False
