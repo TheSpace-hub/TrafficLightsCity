e20752fb4a8f64660d4ba8ba54604cfd346d4bb3
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-20 01:44:45 +0500

    Рамки строительства

--- src/sprites/tile_selection.py
+++ src/sprites/tile_selection.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING, Callable, Optional
 import pygame as pg
 
 from src.sprite import Sprite
@@ -14,9 +14,16 @@ class TileSelection(Sprite):
         self.field: 'Field' = filed
         self.func: Callable[[tuple[int, int]], None] | None = func
         self._visible: bool = False
+        self.limits: pg.Rect = pg.Rect(30, 30, 1860, 910)
 
     def update_view(self):
         self.image = pg.Surface((1920, 1080), pg.SRCALPHA, 32).convert_alpha()
+
+        pg.draw.rect(self.image, (128, 128, 128), self.limits, 3)
+
+        if not self.limits.collidepoint(pg.mouse.get_pos()[0], pg.mouse.get_pos()[1]):
+            return
+
         coord: tuple[int, int] = self.field.get_offset_from_coordinates(self.get_coord())
         polygon: list[tuple[int, int]] = list(
             map(lambda pos: (pos[0] + coord[0], pos[1] + coord[1]), self.field.get_tile_as_polygon()))
@@ -29,7 +36,9 @@ class TileSelection(Sprite):
         self.image = pg.Surface((1920, 1080), pg.SRCALPHA, 32).convert_alpha()
         self._visible = visible
 
-    def get_coord(self) -> tuple[int, int]:
+    def get_coord(self) -> Optional[tuple[int, int]]:
+        if not self.limits.collidepoint(pg.mouse.get_pos()[0], pg.mouse.get_pos()[1]):
+            return None
         return self.field.get_tile_position_by_coordinates(pg.mouse.get_pos())
 
     def update(self):
