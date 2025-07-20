f72d8a9e159c9f852cba334414082de491e54dea
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-20 18:30:10 +0500

    Добавил создание и авто удаление

--- src/sprites/jumpers_group.py
+++ src/sprites/jumpers_group.py
@@ -1,19 +1,19 @@
 from typing import TYPE_CHECKING
 import pygame as pg
 from pygame import SRCALPHA
-
-from src.sprites import Jumper
+from math import sqrt
 
 from src.sprite import Sprite
 
 if TYPE_CHECKING:
     from src.game import Game
+    from src.sprites import Jumper
 
 
 class JumpersGroup(Sprite):
     def __init__(self, game: 'Game'):
         super().__init__(game, (1920, 1080))
-        self.jumpers: list[Jumper] = []
+        self.jumpers: list['Jumper'] = []
 
     def update_view(self):
         self.image = pg.Surface((1920, 1080), SRCALPHA, 32).convert_alpha()
@@ -21,8 +21,15 @@ class JumpersGroup(Sprite):
             jumper.update_view()
             self.image.blit(jumper.image, jumper.rect)
 
+    def add_jumper(self, jumper: 'Jumper'):
+        self.jumpers.append(jumper)
+
     def update(self):
         if len(self.jumpers) > 0:
+            self.jumpers = list(filter(
+                lambda j: j.index < sqrt(2),
+                self.jumpers
+            ))
             for jumper in self.jumpers:
                 jumper.update()
             self.update_view()
