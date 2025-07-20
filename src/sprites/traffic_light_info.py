e7f8e60ee8235035bce6cf6e93da9b57b9b78038
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-20 14:52:04 +0500

    Иконка корректной работы

--- src/sprites/traffic_light_info.py
+++ src/sprites/traffic_light_info.py
@@ -34,7 +34,8 @@ class TrafficLightInfo(Sprite):
             20, 160, self.image.get_size()[0] - 40, 3
         ))
 
-        self.image.blit(self.data.note.get_cover((100, 100)), (30, 30))
+        note_level: int = 4 if self.data.note.get_level() is None else self.data.note.get_level()
+        self.image.blit(self.data.note.get_cover_by_level((100, 100), note_level), (30, 30))
         self.image.blit(Text(self.game, (0, 0), self.data.note.note, 16, (255, 255, 255),
                              align=TextAlign.LEFT).image, (30, 180))
 
