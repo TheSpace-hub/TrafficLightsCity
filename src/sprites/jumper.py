7d596d4b2539919cf60a0d59bc87a51260be0f4f
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-20 18:10:16 +0500

    Приятная анимация

--- src/sprites/jumper.py
+++ src/sprites/jumper.py
@@ -1,4 +1,9 @@
 from typing import TYPE_CHECKING
+from random import uniform
+from math import pi, sqrt, ceil
+
+import pygame as pg
+from pygame import SRCALPHA
 
 from src.sprites import Pixelart
 from src.sprite import Sprite
@@ -8,13 +13,35 @@ if TYPE_CHECKING:
 
 
 class Jumper(Sprite):
-    def __init__(self, game: 'Game', pos: tuple[int, int],
+    def __init__(self, game: 'Game', pos: tuple[int, int], size: tuple[int, int],
                  pixelart: tuple[tuple[tuple[int, int, int, int], ...]]):
-        super().__init__(game, (30, 30), pos)
+        super().__init__(game, (int(size[0] * sqrt(2)), int(size[1] * 2 * sqrt(2))), pos)
         self.pixelart: tuple[tuple[tuple[int, int, int, int], ...]] = pixelart
+        self.index: int = 0
+        self.angle = uniform(-45, 45)
 
     def update_view(self):
-        self.image = Pixelart(self.game, (0, 0), (30, 30), self.pixelart).image
+        self.image = pg.Surface(self.image.get_size(), SRCALPHA, 32).convert_alpha()
+
+        surface = pg.Surface((30, 30), SRCALPHA, 32).convert_alpha()
+        pixel_size: float = min(30 / len(self.pixelart[0]), 30 / len(self.pixelart[1]))
+        alpha = 255 - self.index * 255 / sqrt(2)
+        for y in range(len(self.pixelart)):
+            for x in range(len(self.pixelart[y])):
+                color = (
+                    self.pixelart[y][x][0],
+                    self.pixelart[y][x][1],
+                    self.pixelart[y][x][2],
+                    min(self.pixelart[y][x][3], alpha)
+                )
+                pg.draw.rect(surface, color, pg.Rect(
+                    pixel_size * x, pixel_size * y, ceil(pixel_size), ceil(pixel_size)
+                ))
+
+        self.image.blit(pg.transform.rotate(surface, self.angle),
+                        (0, 43 - self.index * 30 / sqrt(2)))
 
     def update(self):
-        self.update_view()
+        if self.index < sqrt(2):
+            self.update_view()
+            self.index += .05
