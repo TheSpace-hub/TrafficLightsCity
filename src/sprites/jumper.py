0c8fec73968f865e785348c5f141b6f5a0eafa63
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-20 17:30:49 +0500

    Убрал увеличение

--- src/sprites/jumper.py
+++ src/sprites/jumper.py
@@ -1,31 +1,20 @@
-from typing import TYPE_CHECKING, Callable
-from math import ceil
-import pygame as pg
-from pygame import Surface, SRCALPHA
-from PIL import Image
+from typing import TYPE_CHECKING
 
-from src.sprites import Container
+from src.sprites import Pixelart
+from src.sprite import Sprite
 
 if TYPE_CHECKING:
     from src.game import Game
 
 
-class Jumper(Container):
-    def __init__(self, game: 'Game', pos: tuple[int, int], size: tuple[int, int],
-                 placeholder: Callable[[], pg.Surface] | None = None):
-        self.zoom: float = 0
-        super().__init__(game, pos, size, placeholder)
+class Jumper(Sprite):
+    def __init__(self, game: 'Game', pos: tuple[int, int],
+                 pixelart: tuple[tuple[tuple[int, int, int, int], ...]]):
+        super().__init__(game, (30, 30), pos)
+        self.pixelart: tuple[tuple[tuple[int, int, int, int], ...]] = pixelart
 
     def update_view(self):
-        self.image.fill((32, 32, 32))
-        if self.placeholder is not None:
-            placeholder: pg.Surface = self.placeholder()
-            new_size = (
-                placeholder.get_size()[0] * self.zoom ** 2,
-                placeholder.get_size()[1] * self.zoom ** 2,
-            )
-            self.image.blit(
-                pg.transform.scale(placeholder, new_size, None), (3, 3))
+        self.image = Pixelart(self.game, (0, 0), (30, 30), self.pixelart).image
+
     def update(self):
         self.update_view()
-        self.zoom += .01
