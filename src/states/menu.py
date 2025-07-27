dd7ada227538f6dbacf1ec445e11025b835fda07
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-07-27 19:03:13 +0300

    Возможность открыть город

--- src/states/menu.py
+++ src/states/menu.py
@@ -16,11 +16,11 @@ class Menu(State):
         self.add_sprite('create_city', Button(self.game, (510, 460), (900, 70),
                                               InBlockText(self.game, 'Создать город', 16,
                                                           (255, 255, 255)),
-                                              self.on_create_world_button_pressed))
+                                              self.on_create_city_button_pressed))
         self.add_sprite('open_city', Button(self.game, (510, 540), (900, 70),
                                             InBlockText(self.game, 'Открыть город', 16,
                                                         (255, 255, 255)),
-                                            enabled=False))
+                                            self.on_open_city_button_pressed))
         self.add_sprite('traffic_light_editor', Button(self.game, (510, 620), (445, 70),
                                                        InBlockText(self.game, 'Редактор светофоров', 16,
                                                                    (255, 255, 255)),
@@ -47,7 +47,7 @@ class Menu(State):
     def exit(self):
         pass
 
-    def on_create_world_button_pressed(self, status: ButtonStatus):
+    def on_create_city_button_pressed(self, status: ButtonStatus):
         if status == ButtonStatus.PRESSED:
             self.game.change_state('CreateCity')
 
@@ -62,3 +62,7 @@ class Menu(State):
     def on_quit_button_pressed(self, status: ButtonStatus):
         if status == ButtonStatus.PRESSED:
             self.game.quit()
+
+    def on_open_city_button_pressed(self, status: ButtonStatus):
+        if status == ButtonStatus.PRESSED:
+            self.game.change_state('OpenCity')
