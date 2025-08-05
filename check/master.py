d9d517ad44b4f3acfea80baef20f1ea0ca1cdbef
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-05 16:49:40 +0300

    Добавил проверки для светофоров

--- check/master.py
+++ check/master.py
@@ -24,3 +24,39 @@ def check_basic(request: dict, response: dict) -> tuple[bool, Optional[str]]:
     """
 
     return True, None
+
+
+@checker('arrow')
+def check_basic(request: dict, response: dict) -> tuple[bool, Optional[str]]:
+    """
+    Пример проверки запроса на 3-х сегментный светофор со стрелкой и ответа.
+
+    Args:
+        request (dict): Словарь, отправленный долбилкой
+        response (dict):  Словарь, возвращённый с сервиса
+
+    Returns:
+        tuple[bool, str]: Кортеж, где:
+            - bool: Результат проверки (True — успех, False — ошибка).
+            - str | None: Описание ошибки (Используйте None для отсутствия параметра)
+    """
+
+    return True, None
+
+
+@checker('pedestrian_crossing')
+def check_basic(request: dict, response: dict) -> tuple[bool, Optional[str]]:
+    """
+    Пример проверки запроса на светофор для пешеходного перехода и ответа.
+
+    Args:
+        request (dict): Словарь, отправленный долбилкой
+        response (dict):  Словарь, возвращённый с сервиса
+
+    Returns:
+        tuple[bool, str]: Кортеж, где:
+            - bool: Результат проверки (True — успех, False — ошибка).
+            - str | None: Описание ошибки (Используйте None для отсутствия параметра)
+    """
+
+    return True, None
